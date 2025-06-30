a70fe69fd217b6bcf41539799428f710f10872c5
narasimman rajendran <narasimman610@gmail.com>
2025-06-30 10:58:28 +0530

    removed commented lines

--- models/Sample/sample_table.sql
+++ models/Sample/sample_table.sql
@@ -1,16 +1,3 @@
--- models/sample_table.sql
-
--- {{ config(
---     materialized='table',
---     post_hook=[
---         "INSERT INTO {{ this }} (id, name) VALUES (1, 'Alice'), (2, 'Bob')"
---     ]
--- ) }}
-
--- SELECT
---     0 AS id,
---     'init' AS name
--- WHERE 1 = 0  -- Create empty table structure
 
 {{ config(
     materialized='incremental',
