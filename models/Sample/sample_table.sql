fd8eb32663f204891bf973fc04102d6a8e3c0db0
narasimman rajendran <narasimman610@gmail.com>
2025-06-30 10:11:54 +0530

    add sample increamental records

--- models/Sample/sample_table.sql
+++ models/Sample/sample_table.sql
@@ -1,13 +1,28 @@
 -- models/sample_table.sql
 
+-- {{ config(
+--     materialized='table',
+--     post_hook=[
+--         "INSERT INTO {{ this }} (id, name) VALUES (1, 'Alice'), (2, 'Bob')"
+--     ]
+-- ) }}
+
+-- SELECT
+--     0 AS id,
+--     'init' AS name
+-- WHERE 1 = 0  -- Create empty table structure
+
 {{ config(
-    materialized='table',
-    post_hook=[
-        "INSERT INTO {{ this }} (id, name) VALUES (1, 'Alice'), (2, 'Bob')"
-    ]
+    materialized='incremental',
+    unique_key='id'
 ) }}
 
+-- Base select statement to append new rows
 SELECT
-    0 AS id,
-    'init' AS name
-WHERE 1 = 0  -- Create empty table structure
+    {{ current_timestamp() | string }} AS id,  -- Use timestamp or sequence to generate new ID
+    'new_user' AS name
+
+{% if is_incremental() %}
+-- Only run if it's incremental
+WHERE 1 = 1
+{% endif %}
