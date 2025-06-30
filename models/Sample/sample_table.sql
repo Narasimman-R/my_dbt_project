b58ac7c960773f7007dec7b84a90b2a3c6338b4a
narasimman rajendran <narasimman610@gmail.com>
2025-06-30 11:10:26 +0530

    fixed the error type mismatch

--- models/Sample/sample_table.sql
+++ models/Sample/sample_table.sql
@@ -1,15 +1,9 @@
-
 {{ config(
     materialized='incremental',
-    unique_key='id'
+    unique_key='id',
+    schema='default'
 ) }}
 
--- Base select statement to append new rows
 SELECT
-    {{ current_timestamp() | string }} AS id,  -- Use timestamp or sequence to generate new ID
-    'new_user' AS name
-
-{% if is_incremental() %}
--- Only run if it's incremental
-WHERE 1 = 1
-{% endif %}
+  3 AS id,
+  'Charlie' AS name
