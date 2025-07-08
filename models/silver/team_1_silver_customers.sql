c981907550d21decb617ccc8f00c3bb3d30fabbe
narasimman rajendran <narasimman610@gmail.com>
2025-07-08 11:33:11 +0530

    log for SQL as well

--- models/silver/team_1_silver_customers.sql
+++ models/silver/team_1_silver_customers.sql
@@ -23,10 +23,16 @@ SELECT
 FROM ranked_customers
 WHERE row_num = 1;
 
+-- ========= Logging block =========
 {% if execute %}
+  {# Row count from target table #}
   {% set result = run_query("SELECT COUNT(*) AS cnt FROM {{ this }}") %}
   {% if result %}
     {% set row_count = result.columns[0].values()[0] %}
     {{ log("Row count in model: " ~ row_count, info=True) }}
   {% endif %}
+
+  {# SQL code logging #}
+  {% set compiled_sql = to_sql() %}
+  {{ log("Compiled SQL: " ~ compiled_sql, info=True) }}
 {% endif %}
